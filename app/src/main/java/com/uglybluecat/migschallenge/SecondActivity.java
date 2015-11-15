a9b60bdb7acb0a8309c48ff7cfa954b0a124c27e
Robin Spinks <uglybluecat@googlemail.com>
2015-11-15 15:30:25 +0000

    three screens with buttons

--- app/src/main/java/com/uglybluecat/migschallenge/SecondActivity.java
+++ app/src/main/java/com/uglybluecat/migschallenge/SecondActivity.java
@@ -5,16 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class FirstActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_second);
     }
 
-    public void openNextActivity(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void openNextActivity (View view) {
+        Intent intent = new Intent(this, ThirdActivity.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/uglybluecat/migschallenge/ThirdActivity.java
+++ app/src/main/java/com/uglybluecat/migschallenge/ThirdActivity.java
@@ -5,16 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class FirstActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_third);
     }
 
-    public void openNextActivity(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+    public void showToast (View view) {
+    }
+
+    public void closeApp (View view) {
     }
 }
